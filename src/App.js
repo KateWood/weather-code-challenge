c47734cf8e8a38d53b9d9476cc28f1c001b204ba
Kate Wood <katewood611@gmail.com>
2020-08-09 00:13:25 -0400

    adds form - alerts city on form submit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Header from './components/Header/Header';
 import Tile from './components/Tile/Tile';
 import './App.scss';
 
 function App() {
+  const [city, setCity] = useState(null);
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    alert(`Traveling to ${city}`)
+  }
+
   return (
     <div className="app">
       <Header/>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Where are you going?
+          <input
+            type="text"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+          />
+        </label>
+        <input type="submit" value="Submit"/>
+      </form>
       <div className="tile-container">
         <Tile />
         <Tile />
