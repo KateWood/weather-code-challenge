a8722ceb97e92bedc9fdf117f03472d5bff6ec54
Kate Wood <katewood611@gmail.com>
2020-08-09 01:10:12 -0400

    updates form to call open weather API on submit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from './components/Header/Header';
 import Tile from './components/Tile/Tile';
 import './App.scss';
 
 function App() {
-  const [city, setCity] = useState(null);
+  const [city, setCity] = useState('');
+  const [search, setSearch] = useState('');
 
+  // update city as it is typed
+  const handleChange = event => {
+    setCity(event.target.value);
+  }
+
+  // trigger useEffect by updating search on form submit
   const handleSubmit = event => {
     event.preventDefault();
-    alert(`Traveling to ${city}`)
+    setSearch(city);
+    console.log(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ef04c3abf4ce54cd1d370241f0074e94`);
   }
+
+  // prevent useEffect on initial page load
+  const isInitialMount = useRef(true);
+
+  useEffect(() => {
+    if (isInitialMount.current) {
+     isInitialMount.current = false;
+    } else {
+      fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=ef04c3abf4ce54cd1d370241f0074e94`,
+        {
+          method: "GET",
+        }
+      )
+        .then(res => res.json())
+        .then(response => {
+          console.log(response);
+        })
+        .catch(error => console.log(error));
+    }
+  }, [search]);
 
   return (
     <div className="app">
       <Header/>
       <form onSubmit={handleSubmit}>
         <label>
-          Where are you going?
+          What city are you visiting?
           <input
             type="text"
             value={city}
-            onChange={e => setCity(e.target.value)}
+            onChange={handleChange}
           />
         </label>
         <input type="submit" value="Submit"/>
