904e7454a966ab2ca4b97ddbcf7e8480530a91c8
Kate Wood <katewood611@gmail.com>
2020-08-11 01:45:37 -0400

    Updates

---
+++ precache-manifest.138032e0bc7c58e45737835786c4d168.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a827e6c4511c88dc26d496abb8e7e1",
+    "url": "/weather-code-challenge/index.html"
+  },
+  {
+    "revision": "c91c25d5ea35751971ef",
+    "url": "/weather-code-challenge/static/css/main.696497c4.chunk.css"
+  },
+  {
+    "revision": "83d5739e46fb1867a8bc",
+    "url": "/weather-code-challenge/static/js/2.0ac7e42d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-code-challenge/static/js/2.0ac7e42d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c91c25d5ea35751971ef",
+    "url": "/weather-code-challenge/static/js/main.6a20257b.chunk.js"
+  },
+  {
+    "revision": "0054caac358a25ca7615",
+    "url": "/weather-code-challenge/static/js/runtime-main.9b7ed30b.js"
+  },
+  {
+    "revision": "91f71289c879bfd6fec79c4fb602dab4",
+    "url": "/weather-code-challenge/static/media/mittens.91f71289.png"
+  },
+  {
+    "revision": "fd86796a697d71ba2964dd897afbca5e",
+    "url": "/weather-code-challenge/static/media/rainboots.fd86796a.png"
+  },
+  {
+    "revision": "5eb45616ec7d96f47390afe22f742303",
+    "url": "/weather-code-challenge/static/media/shorts.5eb45616.png"
+  },
+  {
+    "revision": "3e0537af48f0e5699acd289933e1c306",
+    "url": "/weather-code-challenge/static/media/snowboots.3e0537af.png"
+  },
+  {
+    "revision": "645c9b493a9c819a287ced5c55b882f5",
+    "url": "/weather-code-challenge/static/media/sun.645c9b49.png"
+  },
+  {
+    "revision": "84cd38d85527cf1c9383f750fb1b152b",
+    "url": "/weather-code-challenge/static/media/sunglasses.84cd38d8.png"
+  },
+  {
+    "revision": "df50de3ad26e341fb4679243f1e039f1",
+    "url": "/weather-code-challenge/static/media/sweater.df50de3a.png"
+  },
+  {
+    "revision": "c75aef2e66b9f1d802d14888d7295135",
+    "url": "/weather-code-challenge/static/media/tShirt.c75aef2e.png"
+  },
+  {
+    "revision": "7c80b1b1e3e8b16c75e8e2fb8c1a3820",
+    "url": "/weather-code-challenge/static/media/umbrella.7c80b1b1.png"
+  }
+]);
